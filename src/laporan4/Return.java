4e826510e8b391eb29024233fd82924474a51909
hasanroo10 <60200112015@uin-alauddin.ac.id>
2015-06-28 17:28:51 +0800

    versi 1.0

---
+++ src/laporan4/Return.java
@@ -0,0 +1,15 @@
+package laporan4;
+
+public class Return 
+{
+	public static void main(String[] args) 
+	{
+	  boolean t=true;
+	  System.out.println("Sebelum return");
+	  
+	  if (t)
+		  return;
+	  	  System.out.println("Pernyataan ini tidak akan dieksekusi");
+	}
+
+}
---
+++ src/laporan4/perulangan.java
@@ -0,0 +1,28 @@
+package laporan4;
+
+public class perulangan {
+
+	public static void main (String[] args) {
+		char jeniskelamin='w';
+		//nested if
+		if (jeniskelamin=='L')
+			System.out.println("Cowok");
+		else if (jeniskelamin=='P')
+			System.out.println("Cewek");
+		else
+			System.out.println("Banci");
+
+	//tenary
+			System.out.println(jeniskelamin=='P'? "Dia Pasti Cowok." : "Dia Pasti Cewek");
+
+	//switch case
+	switch (jeniskelamin){
+		case 'L' : System.out.println("Cowok");
+		break;
+		case 'P' : System.out.println("Cewek");
+		break;
+		default : System.out.println("Banci");
+		break;
+		}
+	}
+}
